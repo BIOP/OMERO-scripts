e629fcd2cf90af9f9decd115bd6a362685caba0c
Rémy Dornier <remy.dornier@epfl.ch>
2022-10-21 15:04:29 +0200

    Add kvp deletion on plates script

---
+++ Fiji/advanced_scripts/DL_delete_kvp_on_plate_and_child.groovy
@@ -0,0 +1,146 @@
+#@String(label="Username") USERNAME
+#@String(label="Password", style='password' , value=PASSWORD , persist=false) PASSWORD
+#@Long(label="Plate ID", value=119273) id
+
+/* 
+ * == INPUTS ==
+ *  - credentials 
+ *  - id
+ *  - object type
+ *  - key and value to add to OMERO object
+ * 
+ * == OUTPUTS ==
+ *  - key-value deletion
+ * 
+ * = DEPENDENCIES =
+ *  - Fiji update site OMERO 5.5-5.6
+ *  - simple-omero-client-5.9.1 or later : https://github.com/GReD-Clermont/simple-omero-client
+ * 
+ * = INSTALLATION = 
+ *  Open Script and Run
+ * 
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV -PTECH - BIOP 
+ * 05.10.2022
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Main. Connect to OMERO, delete all key-values attached to a plate and its respective children and disconnect from OMERO
+ * 
+ */
+ 
+// Connection to server
+host = "omero-server.epfl.ch"
+port = 4064
+
+Client user_client = new Client()
+user_client.connect(host, port, USERNAME, PASSWORD.toCharArray())
+
+if (user_client.isConnected()){
+	println "\nConnected to "+host
+	
+	try{
+		processPlate( user_client, user_client.getPlate(id))
+	} finally{
+		user_client.disconnect()
+		println "Disonnected "+host
+	}
+	
+	println "Key-value pairs deletion for the full plate "+id+" : DONE !"
+	return
+	
+}else{
+	println "Not able to connect to "+host
+}
+
+
+/**
+ *Add a new one to the last key-values
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		repository_wpr : OMERO repository object (image, dataset, project, well, plate, screen)
+ * 
+ * */
+def deleteKVP(user_client, repository_wpr){
+	// get the current key-value pairs
+	List<MapAnnotationWrapper> keyValues = repository_wpr.getMapAnnotations(user_client)
+	
+	// delete key-values											   
+	keyValues.each{user_client.delete(it)}
+}
+
+
+/**
+ * get all images within a well
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		well_wpr_listet_wpr : OMERO list of wells
+ * 
+ * */
+def processWell(user_client, well_wpr_list){		
+	well_wpr_list.each{ well_wpr ->		
+		deleteKVP(user_client, well_wpr)		
+		well_wpr.getWellSamples().each{			
+			deleteKVP(user_client, it.getImage())		
+		}
+	}	
+}
+
+
+/**
+ * get all wells within a plate
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		plate_wpr_list : OMERO list of plates
+ * 
+ * */
+def processPlate(user_client, plate_wpr){
+	deleteKVP(user_client, plate_wpr)
+	processWell(user_client, plate_wpr.getWells(user_client))
+}
+
+
+/*
+ * imports  
+ */
+import fr.igred.omero.*
+import fr.igred.omero.roi.*
+import fr.igred.omero.repository.*
+import fr.igred.omero.annotations.*
+import fr.igred.omero.meta.*
+import omero.gateway.model.DatasetData;
+import omero.model.NamedValue
+import ij.*
+import ij.plugin.*
+import ij.gui.PointRoi
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.awt.Rectangle;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.StringTokenizer;
