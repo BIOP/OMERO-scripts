0c36397d8d4e52602c8f500edf6b3742dede6f16
Rémy Dornier <remy.dornier@epfl.ch>
2022-10-24 16:18:10 +0200

    Add metadata scripts

---
+++ Fiji/metadata/UTILS_meta_change_excitation_wavelength.groovy
@@ -0,0 +1,245 @@
+#@String(label="Username") USERNAME
+#@String(label="Password", style='password' , value=PASSWORD , persist=false) PASSWORD
+#@String(label="Excitation wavelength (nm)", value="405,488,552") channelExcitations
+#@String(label="Object", choices={"image","dataset","project","well","plate","screen"}) object_type
+#@Long(label="ID", value=119273) id
+
+
+/* = CODE DESCRIPTION =
+ * This is a template to interact with OMERO. 
+ * User can specify the ID of an "image","dataset","project","well","plate","screen"
+ * The selected object is then imported in FIJI
+ * 
+ * == INPUTS ==
+ *  - host
+ *  - port
+ *  - credentials 
+ *  - list of excitation wavelentgh separated by a comma
+ *  - object type to process
+ *  - id
+ * 
+ * == OUTPUTS ==
+ *  - update excitation wavelength on OMERO
+ * 
+ * = DEPENDENCIES =
+ *  - simple-omero-client v5.9.1 or later : https://github.com/GReD-Clermont/simple-omero-client
+ * 
+ * = INSTALLATION = 
+ *  Open Script and Run
+ * 
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV -PTECH - BIOP 
+ * 24.10.2022
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+// Connection to server
+host = "omero-server.epfl.ch"
+port = 4064
+
+Client user_client = new Client()
+user_client.connect(host, port, USERNAME, PASSWORD.toCharArray())
+
+
+if (user_client.isConnected()){
+	println "Connected to "+host
+	
+	try{
+		
+		switch (object_type){
+			case "image":	
+				processImage(user_client, user_client.getImage(id))
+				break	
+			case "dataset":
+				processDataset(user_client, user_client.getDataset(id))
+				break
+			case "project":
+				processProject(user_client, user_client.getProject(id))
+				break
+			case "well":
+				processWell(user_client, user_client.getWells(id))
+				break
+			case "plate":
+				processPlate(user_client, user_client.getPlates(id))
+				break
+			case "screen":
+				processScreen(user_client, user_client.getScreens(id))
+				break
+		}
+		
+	} finally{
+		user_client.disconnect()
+		println "Disonnected from "+host
+	}
+	
+	println "Updating metadata on image, id "+id+": DONE !"
+	return
+	
+}else{
+	println "Not able to connect to "+host
+}
+
+
+
+/**
+ * Update channel name for each channel for the current image, based on user input.
+ * 
+ */
+def processImage(user_client, img_wpr){
+	
+	// parse the user input
+	def chExcitationList = channelExcitations.split(",")
+	
+	// get channel metadata
+    def imgChannels = user_client.getMetadata().getChannelData(user_client.getCtx(), img_wpr.getId())
+    
+    // check of the number of channels on the image matches the number of user inputs
+    if(imgChannels.size() == chExcitationList.size()){
+    	println "Processing image : "+img_wpr.getId()
+    	
+    	// loop on channels
+    	[imgChannels, chExcitationList].transpose().each{ch,excitation->
+    		println "Previous excitation wavelength for channel "+ ch.getName() + " : " + ch.getExcitationWavelength(UnitsLength.NANOMETER)
+    		
+    		// change the excitation wavelength for the channel
+    		ch.setExcitationWavelength(new LengthI(Double.parseDouble(excitation),UnitsLength.NANOMETER))
+    		
+    		println "New excitation wavelength for channel "+ ch.getName() + " : " + ch.getExcitationWavelength(UnitsLength.NANOMETER)
+    	}
+    	
+    	// update metadata on OMERO
+    	def IObjectChannels = new ArrayList()
+    	imgChannels.each{IObjectChannels.add(it.asIObject())}
+    	user_client.getGateway().getFacility(DataManagerFacility.class).updateObjects(user_client.getCtx(), IObjectChannels, null);
+    	println "Update image on OMERO done !\n"
+    }
+    else
+    	println "The number of excitation wavelength does not match the number of channels of the image"
+}
+
+/**
+ * Import all images from a dataset in Fiji
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		dataset_wpr : OMERO dataset
+ * 
+ * */
+def processDataset( user_client, dataset_wpr ){
+	dataset_wpr.getImages(user_client).each{ img_wpr ->
+		processImage(user_client , img_wpr)
+	}
+}
+
+
+/**
+ * Import all images from a dataset in Fiji
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		dataset_wpr : OMERO dataset
+ * 
+ * */
+def processProject( user_client, project_wpr ){
+	project_wpr.getDatasets().each{ dataset_wpr ->
+		processDataset(user_client , dataset_wpr)
+	}
+}
+
+
+/**
+ * Import all images from a well in Fiji
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		well_wpr_list : OMERO wells
+ * 
+ * */
+def processWell(user_client, well_wpr_list){		
+	well_wpr_list.each{ well_wpr ->				
+		well_wpr.getWellSamples().each{			
+			processImage(user_client, it.getImage())		
+		}
+	}	
+}
+
+
+/**
+ * Import all images from a plate in Fiji
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		plate_wpr_list : OMERO plates
+ * 
+ * */
+def processPlate(user_client, plate_wpr_list){
+	plate_wpr_list.each{ plate_wpr ->	
+		processWell(user_client, plate_wpr.getWells(user_client))
+	} 
+}
+
+
+/**
+ * Import all images from a screen in Fiji
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		screen_wpr_List : OMERO screens
+ * 
+ * */
+def processScreen(user_client, screen_wpr_list){
+	screen_wpr_list.each{ screen_wpr ->	
+		processPlate(user_client, screen_wpr.getPlates())
+	} 
+}
+
+
+
+/*
+ * imports  
+ */
+import fr.igred.omero.*
+import fr.igred.omero.roi.*
+import fr.igred.omero.repository.*
+import fr.igred.omero.repository.PixelsWrapper
+import fr.igred.omero.annotations.*
+import fr.igred.omero.meta.*
+import omero.gateway.facility.MetadataFacility
+import omero.gateway.facility.DataManagerFacility
+import omero.gateway.facility.BrowseFacility
+import omero.gateway.model.*
+import omero.model.Experimenter.*
+import omero.model.TagAnnotationDataI.*
+import omero.gateway.model.TagAnnotationData.*
+import omero.gateway.model.TagAnnotationData
+import omero.gateway.model.FileAnnotationData.*
+import omero.gateway.model.FileAnnotationData
+import omero.gateway.model.RatingAnnotationData.*
+import omero.gateway.model.RatingAnnotationData
+import omero.model.ExperimenterI.*
+import omero.model.enums.UnitsLength
+import omero.model.enums.UnitsTemperature
+import omero.gateway.model.ImageAcquisitionData
+import ij.*
+import omero.RLong;
+import omero.model.*;
