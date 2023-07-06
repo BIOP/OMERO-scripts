0b49e7555f6d23108cbbce27286c4c7715716b7a
Rémy Dornier <remy.dornier@epfl.ch>
2023-07-06 16:40:17 +0200

    add tags on all images within a container

---
+++ Fiji/tags/UTILS_tag_add_new_on_all_images.groovy
@@ -0,0 +1,220 @@
+#@String(label="Username") USERNAME
+#@String(label="Password", style='password' , value=PASSWORD , persist=false) PASSWORD
+#@Long(label="ID", value=119273) id
+#@String(label="Object", choices={"image","dataset","project","well","plate","screen"}) object_type
+#@String(label="New Tag(s)", value = "new_tag1,new_tag2") tags
+
+
+/* = CODE DESCRIPTION =
+ * This is a template to interact with OMERO. 
+ * It add tags on all images within the selected container
+ * 
+ * == INPUTS ==
+ *  - credentials 
+ *  - id
+ *  - object type
+ *  - tags
+ * 
+ * == OUTPUTS ==
+ * - Add tags on images to OMERO
+ * 
+ * = DEPENDENCIES =
+ *  - Fiji update site OMERO 5.5-5.6
+ *  - simple-omero-client-5.14.0 or later : https://github.com/GReD-Clermont/simple-omero-client
+ * 
+ * = INSTALLATION = 
+ *  Open Script and Run
+ * 
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV -PTECH - BIOP 
+ * 2022-05-18
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+
+/**
+ * Main. Connect to OMERO, process tags and disconnect from OMERO
+ * 
+ */
+ 
+// Connection to server
+host = "omero-server.epfl.ch"
+port = 4064
+
+Client user_client = new Client()
+user_client.connect(host, port, USERNAME, PASSWORD.toCharArray())
+
+if (user_client.isConnected()){
+	println "\nConnected to "+host
+	
+	try{
+		switch (object_type){
+			case "image":	
+				processImage(user_client, user_client.getImage(id))
+				break	
+			case "dataset":
+				processDataset(user_client, user_client.getDataset(id))
+				break
+			case "project":
+				processProject(user_client, user_client.getProject(id))
+				break
+			case "well":
+				processWell(user_client, user_client.getWells(id))
+				break
+			case "plate":
+				processPlate(user_client, user_client.getPlates(id))
+				break
+			case "screen":
+				processScreen(user_client, user_client.getScreens(id))
+				break
+		}
+		println "All tags added for "+object_type+ " "+id
+		
+	} finally{
+		user_client.disconnect()
+		println "Disonnected from "+host
+	}	
+	
+}else{
+	println "Not able to connect to "+host
+}
+
+
+/**
+ * Add a new tags to the object.
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		wpr : OMERO object wrapper (image, dataset, project, well, plate, screen)
+ * 
+ * */
+def processImage(user_client, wpr){
+	
+	def tagsToAdd = []
+	
+	// get existing tags
+	def groupTags = user_client.getTags()
+	def imageTags = wpr.getTags(user_client)
+	
+	// find if the tag to add already exists on OMERO. If yes, they are not added twice
+	tags.split(",").each{tag->
+		// find if the requested tag already exists
+		new_tag = groupTags.find{ it.getName().equals(tag) } ?: new TagAnnotationWrapper(new TagAnnotationData(tag))
+		
+		// add the tag if it is not already the case
+		imageTags.find{ it.getName().equals(new_tag.getName()) } ? println("Tag "+tag+" already attached to the "+object_type) : tagsToAdd.add(new_tag)
+	}
+	
+	println("Adding tags to image :"+wpr.getName())
+	tagsToAdd.each{println(it.getName())}
+	
+	// add all tags to the image
+	wpr.addTags(user_client, (TagAnnotationWrapper[])tagsToAdd.toArray())	
+}
+
+
+/**
+ * get all images within a dataset
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		dataset_wpr : OMERO dataset
+ * 
+ * */
+def processDataset( user_client, dataset_wpr ){
+	def dataset_table = null;
+	dataset_wpr.getImages(user_client).each{ img_wpr ->
+		processImage(user_client , img_wpr)
+	}
+}
+
+
+/**
+ * get all datasets within a project
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		project_wpr : OMERO project
+ * 
+ * */
+def processProject( user_client, project_wpr ){
+	project_wpr.getDatasets().each{ dataset_wpr ->
+		processDataset(user_client , dataset_wpr)
+	}
+}
+
+
+/**
+ * get all images within a well
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		well_wpr_listet_wpr : OMERO list of wells
+ * 
+ * */
+def processWell(user_client, well_wpr_list){	
+	well_wpr_list.each{ well_wpr ->				
+		well_wpr.getWellSamples().each{			
+			processImage(user_client, it.getImage())		
+		}
+	}	
+}
+
+
+/**
+ * get all wells within a plate
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		plate_wpr_list : OMERO list of plates
+ * 
+ * */
+def processPlate(user_client, plate_wpr_list){
+	plate_wpr_list.each{ plate_wpr ->	
+		processWell(user_client, plate_wpr.getWells(user_client))
+	} 
+}
+
+
+/**
+ * get all plates within a screen
+ * 
+ * inputs
+ * 	 	user_client : OMERO client
+ * 		screen_wpr_list : OMERO list of screens
+ * 
+ * */
+def processScreen(user_client, screen_wpr_list){
+	screen_wpr_list.each{ screen_wpr ->	
+		processPlate(user_client, screen_wpr.getPlates())
+	} 
+}
+
+
+/*
+ * imports  
+ */
+import fr.igred.omero.*
+import fr.igred.omero.repository.*
+import fr.igred.omero.annotations.*
+import omero.gateway.model.TagAnnotationData;
