9a39b71ab29dfa4401c8c38373a780662e62647b
Rémy Dornier <remy.dornier@epfl.ch>
2023-07-07 16:07:23 +0200

    Add script to donwload image from OMERO

---
+++ Fiji/image/UTILS_image_download_from_omero.groovy
@@ -0,0 +1,111 @@
+#@String(label="Username") USERNAME
+#@String(label="Password", style='password' , value=PASSWORD , persist=false) PASSWORD
+#@Long(label="Image ID", value=119273) id
+#@File(label="Choose the destination folder", style='directory') dir
+
+
+
+/* = CODE DESCRIPTION =
+ * This is a template to interact with OMERO. 
+ * This script download an image from OMERO. If the selected image is a serie (i.e. part of the fileset), 
+ * then the entire fileset is downloaded.
+ * 
+ * == INPUTS ==
+ *  - credentials 
+ *  - id
+ *  - output folder
+ * 
+ * == OUTPUTS ==
+ *  - original image downloaded from OMERO
+ * 
+ * = DEPENDENCIES =
+ *  - Fiji update site OMERO 5.5-5.6
+ *  - simple-omero-client-5.14.0 or later : https://github.com/GReD-Clermont/simple-omero-client
+ * 
+ * = INSTALLATION = 
+ *  Open Script and Run
+ * 
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV -PTECH - BIOP 
+ * 2023.07.07
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Main. Connect to OMERO, process images and disconnect from OMERO
+ * 
+ */
+
+// Connection to server
+host = "omero-server.epfl.ch"
+port = 4064
+
+Client user_client = new Client()
+user_client.connect(host, port, USERNAME, PASSWORD.toCharArray())
+
+
+if (user_client.isConnected()){
+	println "\nConnected to "+host
+	
+	try{
+	
+		processImage(user_client, user_client.getImage(id), dir)
+		
+	} finally{
+		user_client.disconnect()
+		println "Disonnected from "+host
+	}
+		
+}else{
+	println "Not able to connect to "+host
+}
+
+
+/**
+ * Download an image from OMERO
+ * 
+ * inputs
+ * 		user_client : OMERO client
+ * 		image_wpr : OMERO image
+ * 
+ * */
+def processImage(user_client, image_wpr, dir){
+	try{
+		if(dir.exists()){
+			print "Download image, id " + id + "..."
+	        user_client.getGateway().getFacility(TransferFacility.class).downloadImage(user_client.getCtx(), dir.getAbsolutePath(), image_wpr.getId());
+	        println " DONE !"
+		}
+	    else {
+	        println ("\nThe following path does not exists : "+path+ ". Cannot download the image")
+	    } 
+	}catch(Exception e){
+    	println ("\nERROR during download from OMERO")
+    }
+}
+
+
+/*
+ * imports  
+ */
+import fr.igred.omero.*
+import omero.gateway.facility.TransferFacility
