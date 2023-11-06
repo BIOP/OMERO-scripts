b26fcc73b148196d8e444f5e05495ba3bc61d122
Rémy Dornier <remy.dornier@epfl.ch>
2023-11-06 16:44:41 +0100

    Add script to create new dataset / project

---
+++ Developpers/simple-omero-client/advanced_scripts/DL_Create_New_Dataset.groovy
@@ -0,0 +1,98 @@
+#@String(label="Username") USERNAME
+#@String(label="Password", style='password' , value=PASSWORD , persist=false) PASSWORD
+#@Long(label="Project ID", value=1989) projectId
+#@String(label="Dataset name") datasetName
+
+/* 
+ * == INPUTS ==
+ *  - credentials 
+ *  - Project ID where to create the new dataset
+ *  - Name of the new dataset
+ * 
+ * == OUTPUTS ==
+ *  - new empty dataset on OMERO
+ * 
+ * = DEPENDENCIES =
+ *  - Fiji update site OMERO 5.5-5.6
+ *  - simple-omero-client-5.16.0 or later : https://github.com/GReD-Clermont/simple-omero-client
+ * 
+ * = INSTALLATION = 
+ *  Open Script and Run
+ * 
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV - PTECH - BIOP 
+ * 2023.11.06
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+// Connection to server
+host = "omero-server.epfl.ch"
+port = 4064
+
+Client user_client = new Client()
+user_client.connect(host, port, USERNAME, PASSWORD.toCharArray())
+
+if (user_client.isConnected()){
+	println "Connected to "+host
+	
+	try{
+		def projectWrapper = user_client.getProject(projectId)
+		def datasetWrapper = createOmeroDataset(user_client, projectWrapper, datasetName)
+		
+		println "New dataset '"+datasetWrapper.getName()+"' created : ID = "+datasetWrapper.getId()
+
+	} finally{
+		user_client.disconnect()
+		println "Disconnected from "+host
+	}
+		
+}else{
+	println "Not able to connect to "+host
+}
+
+
+/**
+ * return the dataset wrapper corresonding a new dataset or to an existing dataset, depending on the user choice
+ */
+def createOmeroDataset(user_client, projectWrapper, datasetName){	
+	// create a new dataset
+	def dataset = new DatasetData();
+	dataset.setName(datasetName);
+    
+    // send the dataset on OMERO
+    dataset = user_client.getDm().createDataset(user_client.getCtx(), dataset, projectWrapper.asProjectData())
+    def newId = dataset.getId()
+    
+    // get the corresponding wrapper
+    def datasetWrapper = user_client.getDataset(newId)
+	
+	return datasetWrapper
+}
+
+
+/*
+ * imports  
+ */
+import fr.igred.omero.*
+import fr.igred.omero.repository.*
+import omero.gateway.model.DatasetData
