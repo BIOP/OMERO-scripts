b001f1470f79ea9b8b62882287723ce42be7fde7
Rémy Dornier <remy.dornier@epfl.ch>
2025-03-12 15:28:59 +0100

    Update GUI to select the target group/project/dataset

--- For-developpers/ezomero/upload-comet-images.py
+++ For-developpers/ezomero/upload-comet-images.py
@@ -1,16 +1,26 @@
-
 import ezomero
 import traceback
 from PyQt6.QtWidgets import QLineEdit, QLabel, QFileDialog, QPushButton, QMainWindow, QVBoxLayout, \
-    QWidget, QApplication, QHBoxLayout, QRadioButton, QButtonGroup
+    QWidget, QApplication, QHBoxLayout, QRadioButton, QButtonGroup, QComboBox
+
 
 FONT_SIZE = 'font-size: 14px'
 SEPARATOR = ","
+NEW_PREFIX = "$new$"
+HOST = "omero-server-poc.epfl.ch"
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.is_connected = False
+        self.conn = None
+        self.host = HOST
+        self.project_dict = {}
+        self.dataset_dict = {}
+        self.group_dict = {}
+        self.grp_changed = False
 
         # main window settings
         self.setWindowTitle("Main window title")
@@ -43,39 +53,95 @@ class MainWindow(QMainWindow):
         password_widget.setLayout(password_layout)
         widgets.append(password_widget)
 
-        # # Create radio buttons
-        # self.radio_project_new = QRadioButton("New")
-        # self.radio_project_existing = QRadioButton("Existing")
-        # 
-        # # Set the "Default" option selected by default
-        # self.radio_project_new.setChecked(True)
-        # 
-        # # Group the radio buttons for accessibility
-        # self.radio_group = QButtonGroup()
-        # self.radio_group.addButton(self.radio_project_new)
-        # self.radio_group.addButton(self.radio_project_existing)
+        # load groups
+        self.load_group_button = QPushButton(text="Load OMERO groups")
+        self.load_group_button.setStyleSheet(FONT_SIZE)
+        self.load_group_button.clicked.connect(self.load_groups)
+        widgets.append(self.load_group_button)
+
+        # group fields
+        group_layout = QHBoxLayout()
+        group_label = QLabel("Group")
+        group_label.setStyleSheet(FONT_SIZE)
+        self.group_combo = QComboBox()
+        self.group_combo.setStyleSheet(FONT_SIZE)
+        group_widget = QWidget()
+        group_layout.addWidget(group_label)
+        group_layout.addWidget(self.group_combo)
+        group_widget.setLayout(group_layout)
+        widgets.append(group_widget)
+
+        # connection button
+        self.connect_button = QPushButton(text="Connect")
+        self.connect_button.setStyleSheet(FONT_SIZE)
+        self.connect_button.clicked.connect(self.connect_to_omero)
+        self.connect_button.setEnabled(False)
+        widgets.append(self.connect_button)
 
         # project fields
-        project_layout = QHBoxLayout()
+        project_group_layout = QHBoxLayout()
         project_label = QLabel("Project")
         project_label.setStyleSheet(FONT_SIZE)
+
+        self.radio_project_new = QRadioButton("New")
+        self.radio_project_existing = QRadioButton("Existing")
+        self.radio_project_new.setEnabled(False)
+        self.radio_project_existing.setEnabled(False)
+        self.radio_project_new.setChecked(True)
+        radio_project_group = QButtonGroup()
+        radio_project_group.addButton(self.radio_project_new)
+        radio_project_group.addButton(self.radio_project_existing)
+        project_group_widget = QWidget()
+        project_group_layout.addWidget(project_label)
+        project_group_layout.addWidget(self.radio_project_new)
+        project_group_layout.addWidget(self.radio_project_existing)
+        project_group_widget.setLayout(project_group_layout)
+        widgets.append(project_group_widget)
+
+        project_layout = QHBoxLayout()
         self.project = QLineEdit()
+        self.project.setEnabled(False)
         self.project.setStyleSheet(FONT_SIZE)
+        self.project_combo = QComboBox()
+        self.project_combo.setEnabled(False)
+        self.project_combo.setStyleSheet(FONT_SIZE)
+        self.project_combo.currentTextChanged.connect(self.project_text_changed)
         project_widget = QWidget()
-        project_layout.addWidget(project_label)
         project_layout.addWidget(self.project)
+        project_layout.addWidget(self.project_combo)
         project_widget.setLayout(project_layout)
         widgets.append(project_widget)
 
         # dataset fields
-        dataset_layout = QHBoxLayout()
+        dataset_group_layout = QHBoxLayout()
         dataset_label = QLabel("Dataset")
         dataset_label.setStyleSheet(FONT_SIZE)
+
+        self.radio_dataset_new = QRadioButton("New")
+        self.radio_dataset_existing = QRadioButton("Existing")
+        self.radio_dataset_existing.setEnabled(False)
+        self.radio_dataset_new.setEnabled(False)
+        self.radio_dataset_new.setChecked(True)
+        radio_dataset_group = QButtonGroup()
+        radio_dataset_group.addButton(self.radio_dataset_new)
+        radio_dataset_group.addButton(self.radio_dataset_existing)
+        dataset_group_widget = QWidget()
+        dataset_group_layout.addWidget(dataset_label)
+        dataset_group_layout.addWidget(self.radio_dataset_new)
+        dataset_group_layout.addWidget(self.radio_dataset_existing)
+        dataset_group_widget.setLayout(dataset_group_layout)
+        widgets.append(dataset_group_widget)
+
+        dataset_layout = QHBoxLayout()
         self.dataset = QLineEdit()
+        self.dataset.setEnabled(False)
         self.dataset.setStyleSheet(FONT_SIZE)
+        self.dataset_combo = QComboBox()
+        self.dataset_combo.setEnabled(False)
+        self.dataset_combo.setStyleSheet(FONT_SIZE)
         dataset_widget = QWidget()
-        dataset_layout.addWidget(dataset_label)
         dataset_layout.addWidget(self.dataset)
+        dataset_layout.addWidget(self.dataset_combo)
         dataset_widget.setLayout(dataset_layout)
         widgets.append(dataset_widget)
 
@@ -84,14 +150,16 @@ class MainWindow(QMainWindow):
         folder_label = QLabel("Image path")
         folder_label.setStyleSheet(FONT_SIZE)
         self.folder = QLineEdit()
+        self.folder.setEnabled(False)
         self.folder.setStyleSheet(FONT_SIZE)
-        folder_button = QPushButton(text="Choose")
-        folder_button.clicked.connect(self.open_file_chooser)
-        folder_button.setStyleSheet(FONT_SIZE)
+        self.folder_button = QPushButton(text="Choose")
+        self.folder_button.setEnabled(False)
+        self.folder_button.clicked.connect(self.open_file_chooser)
+        self.folder_button.setStyleSheet(FONT_SIZE)
         folder_widget = QWidget()
         folder_layout.addWidget(folder_label)
         folder_layout.addWidget(self.folder)
-        folder_layout.addWidget(folder_button)
+        folder_layout.addWidget(self.folder_button)
         folder_widget.setLayout(folder_layout)
         widgets.append(folder_widget)
         
@@ -100,14 +168,16 @@ class MainWindow(QMainWindow):
         att_label = QLabel("Attachment")
         att_label.setStyleSheet(FONT_SIZE)
         self.att = QLineEdit()
+        self.att.setEnabled(False)
         self.att.setStyleSheet(FONT_SIZE)
-        att_button = QPushButton(text="Choose")
-        att_button.clicked.connect(self.open_multiple_file_chooser)
-        att_button.setStyleSheet(FONT_SIZE)
+        self.att_button = QPushButton(text="Choose")
+        self.att_button.setEnabled(False)
+        self.att_button.clicked.connect(self.open_multiple_file_chooser)
+        self.att_button.setStyleSheet(FONT_SIZE)
         att_widget = QWidget()
         att_layout.addWidget(att_label)
         att_layout.addWidget(self.att)
-        att_layout.addWidget(att_button)
+        att_layout.addWidget(self.att_button)
         att_widget.setLayout(att_layout)
         widgets.append(att_widget)
 
@@ -125,6 +195,11 @@ class MainWindow(QMainWindow):
         button_widget.setLayout(button_layout)
         widgets.append(button_widget)
 
+        self.radio_project_existing.toggled.connect(self.existing_project_selected)
+        self.radio_project_new.toggled.connect(self.new_project_selected)
+        self.radio_dataset_existing.toggled.connect(self.existing_dataset_selected)
+        self.radio_dataset_new.toggled.connect(self.new_dataset_selected)
+
         # building the main GUI
         for w in widgets:
             main_layout.addWidget(w)
@@ -136,44 +211,197 @@ class MainWindow(QMainWindow):
         # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
+
     def close_app(self):
         self.close()
 
+
     def run_app(self):
-        username = self.username.text()
-        password = self.password.text()
-        project = self.project.text()
-        dataset = self.dataset.text()
+        if self.radio_project_new.isChecked():
+            project = NEW_PREFIX + self.project.text()
+            self.project_dict[project] = -1
+        else:
+            project = self.project_combo.currentText()
+
+        if self.radio_dataset_new.isChecked():
+            dataset = NEW_PREFIX + self.dataset.text()
+            self.dataset_dict[dataset] = -1
+        else:
+            dataset = self.dataset_combo.currentText()
+
         image_path = self.folder.text()
         attachments = self.att.text()
         self.close()
-        run_script(username, password, project, dataset, image_path, attachments)
+        run_script(self.conn, self.host, project, self.project_dict, dataset,
+                   self.dataset_dict, image_path, attachments)
+
 
     def open_file_chooser(self):
         rsp_path_list, rsp_filter = QFileDialog.getOpenFileName(parent=self, caption="select an image")
         self.folder.setText(str(rsp_path_list))
-        
+
+
     def open_multiple_file_chooser(self):
         rsp_path_list, rsp_filter = QFileDialog.getOpenFileNames(parent=self, caption="select one or more files")
         path_list = SEPARATOR.join(rsp_path_list)
         self.att.setText(str(path_list))
 
 
-def run_script(username, password, project_name, dataset_name, image_path, attachments):
-    host = "omero-server-poc.epfl.ch"
-    conn = ezomero.connect(username, password, group="", host=host, port=4064, secure=True)
-    conn.connect()
+    def connect_to_omero(self):
+        username = self.username.text()
+        password = self.password.text()
+        group = self.group_combo.currentText()
+        self.conn.close()
 
-    if conn.isConnected():
+        self.conn = ezomero.connect(username, password, group=f"{group}", host=self.host, port=4064, secure=True)
+
+        if self.conn.isConnected():
+            self.is_connected = True
+            self.att.setEnabled(True)
+            self.folder.setEnabled(True)
+            self.folder_button.setEnabled(True)
+            self.att_button.setEnabled(True)
+            self.project.setEnabled(False)
+            self.dataset.setEnabled(True)
+            self.password.setEnabled(False)
+            self.password.setText("")
+            self.username.setEnabled(False)
+            self.connect_button.setEnabled(False)
+            self.group_combo.setEnabled(False)
+            self.radio_project_new.setEnabled(True)
+            self.radio_dataset_existing.setEnabled(True)
+            self.radio_dataset_new.setEnabled(True)
+            self.radio_project_existing.setEnabled(True)
+            self.new_project_selected()
+
+
+    def load_groups(self):
+        username = self.username.text()
+        password = self.password.text()
+        self.conn = ezomero.connect(username, password, group="", host=self.host, port=4064, secure=True)
+
+        if self.conn.isConnected():
+            group_names = sorted(self.list_groups(self.conn))
+            for group_name in group_names:
+                self.group_combo.addItem(group_name)
+
+            if len(group_names) > 0:
+                group_name = self.conn.getEventContext().groupName
+                self.group_combo.setCurrentText(group_name)
+                self.load_group_button.setEnabled(False)
+                self.connect_button.setEnabled(True)
+
+
+    def list_groups(self, conn):
+        # Retrieve the services we are going to use
+        admin_service = conn.getAdminService()
+
+        ec = admin_service.getEventContext()
+        groups = [admin_service.getGroup(v) for v in ec.memberOfGroups]
+        group_names = []
+        for group in groups:
+            if group.id.val in [0, 1, 2]:
+                continue
+            group_names.append(group.name.val)
+            self.group_dict[group.name.val] = group.id.val
+        return group_names
+
+
+    def list_projects(self, conn):
+        projects = conn.getObjects("Project", opts={'owner': conn.getUser().getId()})
+        project_names = []
+        for project in projects:
+            project_names.append(project.getName())
+            self.project_dict[project.getName()] = project.getId()
+        return project_names
+
+
+    def list_datasets(self, conn, project_id):
+        project = conn.getObject("Project", project_id)
+        dataset_names = []
+        for dataset in project.listChildren():
+            dataset_names.append(dataset.getName())
+            self.dataset_dict[dataset.getName()] = dataset.getId()
+        return dataset_names
+
+
+    def new_project_selected(self):
+        self.project.setEnabled(self.radio_project_new.isChecked())
+        self.project_combo.setEnabled(not self.radio_project_new.isChecked())
+        self.dataset.setEnabled(self.radio_project_new.isChecked())
+        self.dataset_combo.setEnabled(not self.radio_project_new.isChecked())
+        self.radio_dataset_new.setEnabled(not self.radio_project_new.isChecked())
+        self.radio_dataset_existing.setEnabled(not self.radio_project_new.isChecked())
+        self.radio_dataset_new.setChecked(self.radio_project_new.isChecked())
+        self.radio_dataset_existing.setChecked(not self.radio_project_new.isChecked())
+
+
+    def existing_project_selected(self):
+        self.project.setEnabled(not self.radio_project_existing.isChecked())
+        self.project_combo.setEnabled(self.radio_project_existing.isChecked())
+        self.dataset.setEnabled(self.radio_project_existing.isChecked())
+        self.dataset_combo.setEnabled(not self.radio_project_existing.isChecked())
+        self.radio_dataset_new.setEnabled(self.radio_project_existing.isChecked())
+        self.radio_dataset_new.setChecked(self.radio_project_existing.isChecked())
+        self.radio_dataset_existing.setEnabled(self.radio_project_existing.isChecked())
+        self.radio_dataset_existing.setChecked(not self.radio_project_existing.isChecked())
+
+
+    def new_dataset_selected(self):
+        self.dataset.setEnabled(self.radio_dataset_new.isChecked())
+        self.dataset_combo.setEnabled(not self.radio_dataset_new.isChecked())
+
+
+    def existing_dataset_selected(self):
+        self.dataset.setEnabled(not self.radio_dataset_existing.isChecked())
+        self.dataset_combo.setEnabled(self.radio_dataset_existing.isChecked())
+
+
+    def group_text_changed(self, s):
+        self.grp_changed = True
+        group_id = self.group_dict[s]
+        self.conn.SERVICE_OPTS.setOmeroGroup(group_id)
+        project_names = sorted(self.list_projects(self.conn))
+        self.project_combo.clear()
+        for project_name in project_names:
+            self.project_combo.addItem(project_name)
+        if len(project_names) > 0:
+            self.project_combo.setCurrentText(project_names[0])
+            self.grp_changed = False
+            self.project_text_changed(project_names[0])
+
+
+    def project_text_changed(self, s):
+        if not self.grp_changed:
+            dataset_names = sorted(self.list_datasets(self.conn, self.project_dict[s]))
+            self.dataset_combo.clear()
+            for dataset_name in dataset_names:
+                self.dataset_combo.addItem(dataset_name)
+            if len(dataset_names) > 0:
+                self.dataset_combo.setCurrentText(dataset_names[0])
+
+
+def run_script(conn, host, project_name, project_dict, dataset_name, dataset_dict, image_path, attachments):
+
+    if conn is not None and conn.isConnected():
         print(f"Connected to {host}")
         try:
-            if project_name != "":
-                print(f"Creating project '{project_name}'")
-                project_id = ezomero.post_project(conn, project_name)
+            if project_name is not None and project_name != "":
+                if project_dict[project_name] < 0:
+                    real_name = project_name.replace(NEW_PREFIX, "")
+                    print(f"Creating project '{real_name}'")
+                    project_id = ezomero.post_project(conn, real_name)
+                else:
+                    project_id = project_dict[project_name]
 
-                if dataset_name != "":
-                    print(f"Creating dataset '{dataset_name}'")
-                    dataset_id = ezomero.post_dataset(conn, dataset_name, project_id=project_id)
+                if dataset_name is not None and dataset_name != "":
+                    if dataset_dict[dataset_name] < 0:
+                        real_name = dataset_name.replace(NEW_PREFIX, "")
+                        print(f"Creating dataset '{real_name}'")
+                        dataset_id = ezomero.post_dataset(conn, real_name, project_id=project_id)
+                    else:
+                        dataset_id = dataset_dict[dataset_name]
+
                     image_ids = ezomero.ezimport(conn, image_path, dataset=dataset_id)
 
                     if len(image_ids) > 0:
